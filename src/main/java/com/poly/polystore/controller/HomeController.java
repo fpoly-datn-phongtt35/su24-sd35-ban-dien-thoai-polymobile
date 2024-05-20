27402a58b53befd24903bce68ac3919916204228
ChucNB <nbchuc@hotmail.com>
2024-05-20 13:17:28 +0700

    no message

--- src/main/java/com/poly/polystore/controller/HomeController.java
+++ src/main/java/com/poly/polystore/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    @ResponseBody
     public String home() {
-        return "home";
+        return "/admin/san_pham_chi_tiet/mau_sac/add";
     }
 }
