937f78c934d51e1b444f7f18feff376cb5d160b7
ChucNB <nbchuc@hotmail.com>
2024-05-29 15:49:13 +0700

    CRUD màu sắc

--- src/main/java/com/poly/polystore/controller/MauSacController.java
+++ src/main/java/com/poly/polystore/controller/MauSacController.java
@@ -4,14 +4,16 @@ import com.poly.polystore.dto.MauSacDto;
 
 import com.poly.polystore.entity.MauSac;
 import com.poly.polystore.repository.MauSacRepository;
+import com.poly.polystore.ultils.Utils;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.modelmapper.internal.TypeResolvingList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +21,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
-
-import static java.rmi.server.LogStream.log;
+import java.util.stream.Collectors;
 
 @Controller
 public class MauSacController {
@@ -34,45 +36,68 @@ public class MauSacController {
 
     @GetMapping("/admin/san-pham-chi-tiet/mau-sac")
     public String ui(Model model) {
-        return "/admin/san-pham-chi-tiet/mau-sac/mau-sac";
+        return "/admin/san-pham-chi-tiet/mau-sac";
     }
 
     @ResponseBody
     @GetMapping("/api/v1/san-pham-chi-tiet/mau-sac")
     public List<MauSacDto> getAll(Model model) {
-        return modelMapper.map(mauSacRepository.findAll(),new TypeToken<List<MauSacDto>>(){}.getType());
+        return modelMapper.map(mauSacRepository.findAll(), new TypeToken<List<MauSacDto>>() {
+        }.getType());
     }
+
     @ResponseBody
     @PostMapping("/api/v1/san-pham-chi-tiet/mau-sac")
     public MauSacDto update(@Valid @RequestBody MauSacDto mauSacDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, bindingResult.getFieldError().getDefaultMessage());
         }
-        if(mauSacDto.getId() == null||!mauSacRepository.existsById(mauSacDto.getId())){
+        if (mauSacDto.getId() == null || !mauSacRepository.existsById(mauSacDto.getId())) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
         }
         mauSacRepository.save(modelMapper.map(mauSacDto, MauSac.class));
         return mauSacRepository.findById(mauSacDto.getId()).map((element) -> modelMapper.map(element, MauSacDto.class)).get();
     }
+
     @ResponseBody
     @PutMapping("/api/v1/san-pham-chi-tiet/mau-sac")
-    public MauSacDto add(@Valid @RequestBody MauSacDto mauSacDto, BindingResult bindingResult) {
+    public List<MauSacDto> add(@Valid @RequestBody List<MauSacDto> lstMSDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, bindingResult.getFieldError().getDefaultMessage());
         }
-
-        mauSacRepository.save(modelMapper.map(mauSacDto, MauSac.class));
-        return mauSacRepository.findByMa(mauSacDto.getMa()).map((element) -> modelMapper.map(element, MauSacDto.class)).get();
+        List<MauSac> lstMS = lstMSDTO.stream().map((element) -> modelMapper.map(element, MauSac.class)).collect(Collectors.toList());
+        List<MauSacDto> lstRP = mauSacRepository.saveAll(lstMS).stream().map((element) -> modelMapper.map(element, MauSacDto.class)).collect(Collectors.toList());
+        return lstRP;
     }
+
     @ResponseBody
     @DeleteMapping("/api/v1/san-pham-chi-tiet/mau-sac")
     public MauSacDto delete(@RequestParam(name = "id") Integer id) {
-        if (id==null||!mauSacRepository.existsById(id)) {
+        if (id == null || !mauSacRepository.existsById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
         }
-        var ms= mauSacRepository.findById(id).map((element) -> modelMapper.map(element, MauSacDto.class)).get();
+        var ms = mauSacRepository.findById(id).map((element) -> modelMapper.map(element, MauSacDto.class)).get();
         mauSacRepository.deleteById(id);
         return ms;
     }
 
+//    @GetMapping("/api/v1/san-pham-chi-tiet/mau-sac/export-excel")
+//    public ResponseEntity<byte[]> getAll(Model model) {
+//        return modelMapper.map(mauSacRepository.findAll(), new TypeToken<List<MauSacDto>>() {
+//        }.getType());
+//    }
+@GetMapping("/admin/san-pham-chi-tiet/mau-sac/export-excel")
+public ResponseEntity<byte[]> exportToExcel() throws IOException {
+    List<MauSac> mauSacList = mauSacRepository.findAll();
+    Utils<MauSac> utils = new Utils();
+    ByteArrayInputStream in = utils.printAllFields(mauSacList);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Disposition", "attachment; filename=mau_sac.xlsx");
+
+    return ResponseEntity.ok()
+            .headers(headers)
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(in.readAllBytes());
+}
 }
--- src/main/resources/static/js/demo/util.js
+++ src/main/resources/static/js/demo/util.js
@@ -57,8 +57,55 @@ const clearForm = () => {
         // Nếu có, loại bỏ class 'input' từ tất cả các phần tử
         $('input').val('');
     }
-
-
 }
+function dataToJson(event) {
+    return new Promise((resolve, reject) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
 
+        reader.onload = function(e) {
+            try {
+                const data = new Uint8Array(e.target.result);
+                const workbook = XLSX.read(data, { type: 'array' });
+
+                // Giả sử dữ liệu nằm trong sheet đầu tiên
+                const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+                const jsonData = XLSX.utils.sheet_to_json(firstSheet);
+
+                // Chuyển JSON thành chuỗi và lưu vào input ẩn
+                // document.getElementById('json-data').value = JSON.stringify(jsonData);
+                resolve(jsonData)
+                console.log(jsonData)
+                event.target.files[0].value=""
+            }catch (e) {
+                reject(e)
+            }
+        };
+
+
+        reader.readAsArrayBuffer(file);
+    })
+}
+const showConfirm = (title, message) => {
+    console.log("showConfirm");
+
+    return new Promise((resolve, reject) => {
+        $('#modal-confirm-title').html(title);
+        $('#modal-confirm-message').html(message);
+        $('#modal-confirm').modal('show');
+
+        // Hủy bỏ các sự kiện click trước đó để tránh đăng ký nhiều lần
+        $('#modal-confirm-confirm').off('click').on('click', () => {
+            console.log("OK");
+            $('#modal-confirm').modal('hide');
+            resolve(true);
+        });
+
+        $('#modal-confirm-cancel').off('click').on('click', () => {
+            console.log("Cancel");
+            $('#modal-confirm').modal('hide');
+            reject(false);
+        });
+    });
+}
 
